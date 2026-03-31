bcb4dca728ace47829e4528a1e77890059078efd
shaheersha7 <shaheershat@gmail.com>
2026-03-31 11:26:40 +0530

    Fix startServer function structure - move app setup inside function

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ async function startServer() {
   const app = express();
   app.use(express.json());
   app.use(cors());
-}
+  
   // Enable CORS for all routes
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
